e2a45e68b47db47870612f083bf00f04259cdc98
vayinala555 <138670313+vayinala555@users.noreply.github.com>
2024-08-10 22:57:23 +0530

    Getting Middle Element From Linked List Approach2

--- Test/src/test/practice/ds/MyLinkedList.java
+++ Test/src/test/practice/ds/MyLinkedList.java
@@ -2,68 +2,87 @@ package test.practice.ds;
 
 public class MyLinkedList {
 
-	Node head;
+    Node head;
 
-	public static MyLinkedList insert(MyLinkedList list, String data)
-	{
-		// creating new node with input param 
-		Node newNode = new Node(data);
-		newNode.next = null;
-		if (list.head == null) {
-			list.head = newNode;
-		} else {     // finding last node where next contains value as null
-			Node lstNode = list.head;
-			while (lstNode.next != null) {
-				lstNode = lstNode.next;
-			}
-			lstNode.next = newNode;
-		}
-		
-		return list;
-	}
-	
-	private void getValues(MyLinkedList myList) {
-		Node current_node = myList.head;
-		while (current_node != null) {
-			System.out.println(current_node.data);
-			current_node = current_node.next;
-		}
+    public static MyLinkedList insert(MyLinkedList list, String data) {
+        // creating new node with input param 
+        Node newNode = new Node(data);
+        newNode.next = null;
+        if (list.head == null) {
+            list.head = newNode;
+        } else {     // finding last node where next contains value as null
+            Node lstNode = list.head;
+            while (lstNode.next != null) {
+                lstNode = lstNode.next;
+            }
+            lstNode.next = newNode;
+        }
 
-	}
+        return list;
+    }
 
-	public Node getMiddleNode(MyLinkedList list) {
-		Node currentNode = list.head;
-		if(currentNode.next == null) {
-			return currentNode;
-		}
-		else {
-			int counter = 1;
-			Node middleNode = list.head;
-			while (head != null) {
-				if (counter % 2 == 0) {
-					middleNode = middleNode.next;
-				}
-				head = head.next;
-				counter++;
+    private void getValues(MyLinkedList myList) {
+        Node current_node = myList.head;
+        while (current_node != null) {
+            System.out.println(current_node.data);
+            current_node = current_node.next;
+        }
 
-			}
-			return middleNode;
-		}
-	}
-	public static void main(String[] args) {
-		MyLinkedList myList = new MyLinkedList();
-		myList.insert(myList, "1");
-		myList.insert(myList, "2");
-		myList.insert(myList, "3");
-		myList.insert(myList, "4");
-		myList.insert(myList, "5");
-		myList.insert(myList, "6");
+    }
 
-		System.out.println(myList);
-		
-		// displaying values
-		myList.getValues(myList);
-		System.out.println(" middle node value is "+myList.getMiddleNode(myList).data);
-	}
+    public Node getMiddleNode(MyLinkedList list) {
+        Node currentNode = list.head;
+        Node traversingNode = list.head;
+        if (currentNode.next == null) {
+            return currentNode;
+        } else {
+            int counter = 1;
+            Node middleNode = list.head;
+            while (traversingNode != null) {
+                if (counter % 2 == 0) {
+                    middleNode = middleNode.next;
+                }
+                traversingNode = traversingNode.next;
+                counter++;
 
+            }
+            return middleNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList myList = new MyLinkedList();
+        myList.insert(myList, "1");
+        myList.insert(myList, "2");
+        myList.insert(myList, "3");
+        myList.insert(myList, "4");
+        myList.insert(myList, "5");
+        myList.insert(myList, "6");
+
+        System.out.println(myList);
+
+        // displaying values
+        myList.getValues(myList);
+        Node middleNode = myList.getMiddleNode(myList);
+        System.out.println(" Approach 1 middle node value is " + middleNode.data);
+
+        Node middleNodeApproach2 = myList.getMiddleNodeApproach2(myList);
+        System.out.println("middleNodeApproach2 = " + middleNodeApproach2.data);
+                
+    }
+
+    /* In this approach we use slow and fast pointers concept it means 
+     slow pointer moves one step forward, fast pointer moves 2 steps at a time.
+    when the fast pointer reaches last element then slow pointer is reaches to middle position
+	*/
+    public Node getMiddleNodeApproach2(MyLinkedList list) {
+        Node slowPointer = list.head;
+        Node fastPointer = list.head;
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        return slowPointer;
+
+    }
 }
